e1108cdae1133fbc1e1e0e86050029e5d20dc3bf
alihemmatnia <ali.hemmatnia@gmail.com>
2024-08-24 12:41:51 +0330

    sql db exception

--- utils/Sql.py
+++ utils/Sql.py
@@ -15,19 +15,23 @@ class SQL:
         self.cursor = self.conn.cursor()
 
     def drop_all_database(self):
-        db_names = """
+        try:
+            db_names = """
             SELECT name 
             FROM sys.databases 
             WHERE name NOT IN ('master', 'tempdb', 'model', 'msdb')
-        """
-        self.cursor.execute(db_names)
-        databases = self.cursor.fetchall()
-        for db in databases:
-            db_name = db[0]
-            drop_database_sql = f"DROP DATABASE IF EXISTS [{db_name}]"
-            self.cursor.execute(drop_database_sql)
-            print(f"Database {db_name} has been dropped successfully.")
-        self.cursor.close()
-        self.conn.commit()
-        self.conn.close()
-        print(f"Done drop sql database.")
+            """
+            self.cursor.execute(db_names)
+            databases = self.cursor.fetchall()
+            for db in databases:
+                db_name = db[0]
+                drop_database_sql = f"DROP DATABASE IF EXISTS [{db_name}]"
+                self.cursor.execute(drop_database_sql)
+                print(f"Database {db_name} has been dropped successfully.")
+            self.cursor.close()
+            self.conn.commit()
+            self.conn.close()
+            print(f"Done drop sql database.")
+        except:
+            print("Error in SqlServer")
+            
